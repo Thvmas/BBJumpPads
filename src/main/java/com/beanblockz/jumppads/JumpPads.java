740846fad8b85e627461d897e5a76aea33aecf96
SpongyBacon <spongybacon@hotmail.co.uk>
2016-08-02 21:13:32 +0100

    First commit

---
+++ src/main/java/com/beanblockz/jumppads/JumpPads.java
@@ -0,0 +1,12 @@
+package com.beanblockz.jumppads;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class JumpPads extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
